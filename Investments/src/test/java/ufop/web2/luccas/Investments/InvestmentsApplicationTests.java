298cb577b76dd9adbc79bc158d33a58337552321
Luccas Carneiro <luccas.carneiro@terceiroslinx.com.br>
2025-08-10 23:22:53 -0300

    first commit

---
+++ Investments/src/test/java/ufop/web2/luccas/Investments/InvestmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package ufop.web2.luccas.Investments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Nameserver/src/main/java/ufop/web2/luccas/Nameserver/NameserverApplication.java
@@ -0,0 +1,13 @@
+package ufop.web2.luccas.Nameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameserverApplication.class, args);
+	}
+
+}
